e2b0edfcc846e0484dcbf670f5be2138692c4295
mohammedyasin021 <160766863+mohammedyasin021@users.noreply.github.com>
2025-09-10 16:54:11 +0300

    create sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portofolio_app/views/home_view.dart';
+import 'package:portofolio_app/views/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeView(),
+      home: const SignInPage(),
     );
   }
 }
